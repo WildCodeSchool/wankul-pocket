c76f7f5449df7f2a4bdeadbe6cba42e64cf8e088
Jordan-182 <Jordan.P62@outlook.fr>
2025-06-16 11:23:09 +0200

    added images

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -5,14 +5,29 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.globalHeader}>
-      <Link href={""}></Link>
+      <Link href={"/Profil"} className={styles.link}>
+        <Image
+          src={"/banana.png"}
+          alt="Avatar de profil"
+          height={50}
+          width={50}
+        />
+      </Link>
       <Image
         src={"/headerLogo.png"}
         alt="Logo Wankul Pocket"
         height={50}
         width={125}
       />
-      <Link href={""}></Link>
+      <Link href={"/Booster"} className={styles.link}>
+        <Image
+          src={"/banana.png"}
+          alt="Compteur de bananes"
+          height={50}
+          width={50}
+        />
+        <p>{}</p>
+      </Link>
     </header>
   );
 }
