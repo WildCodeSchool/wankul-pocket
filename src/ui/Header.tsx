192b392e742cbab55ed9ea39694535549e17dfe9
Jordan-182 <Jordan.P62@outlook.fr>
2025-06-16 20:11:05 +0200

    header desktop done

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -3,10 +3,12 @@
 import { useUserContext } from "@/context/UserContext";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./Header.module.css";
 
 export default function Header() {
   const { user } = useUserContext();
+  const pathname = usePathname();
 
   console.log(user);
   if (!user) {
@@ -33,7 +35,7 @@ export default function Header() {
           width={50}
         />
       </Link>
-      <Link href={"/homepage"}>
+      <Link href={"/homepage"} className={styles.headerLogo}>
         <Image
           src={"/headerLogo.png"}
           alt="Logo Wankul Pocket"
@@ -50,6 +52,46 @@ export default function Header() {
         />
         <p>{user?.bananas}</p>
       </Link>
+      <nav className={styles.headerNav}>
+        <ul className={styles.linksContainer}>
+          <li className={styles.listItem}>
+            <Link
+              href="/collection"
+              className={
+                pathname === "/collection" ? styles.active : styles.link
+              }
+            >
+              Collection
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link
+              href="/echange"
+              className={pathname === "/echange" ? styles.active : styles.link}
+            >
+              Echange
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link
+              href="/amis"
+              className={pathname === "/amis" ? styles.active : styles.link}
+            >
+              Amis
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link
+              href="/objectifs"
+              className={
+                pathname === "/objectifs" ? styles.active : styles.link
+              }
+            >
+              Objectifs
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
