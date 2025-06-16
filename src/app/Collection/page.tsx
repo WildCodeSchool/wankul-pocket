4735d208b1da070f2a83079a9545e455ecd8dbae
Jordan-182 <152084774+Jordan-182@users.noreply.github.com>
2025-06-16 11:53:13 +0200

    Delete src/app/Collection directory

--- src/app/Collection/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Collection() {
-  return (
-    <div>
-      <h1>Ma Collection</h1>
-      <p>Bienvenue dans votre collection !</p>
-      <p>Vous pouvez y gérer vos articles préférés.</p>
-    </div>
-  );
-}
