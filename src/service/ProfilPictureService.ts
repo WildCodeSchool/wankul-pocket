4ce14de110d98f8e257d447e80a50eeafd7edf5a
Jordan-182 <jordan.p62@outlook.fr>
2025-06-17 14:28:26 +0200

    removed everything useless for profilPictures

--- src/service/ProfilPictureService.ts
+++ src/service/ProfilPictureService.ts
@@ -1,28 +1,5 @@
-import { addProfilPicture } from "@/lib/profilPicture/addProfilPicture";
-import { deleteProfilPicture } from "@/lib/profilPicture/deleteProfilPicture";
-import { getOne } from "@/lib/profilPicture/getProfilPicture";
 import { getProfilPictures } from "@/lib/profilPicture/getProfilPictures";
-import { patchProfilPicture } from "@/lib/profilPicture/patchProfilPicture";
-import { ProfilPictureModel } from "@/model/ProfilPictureModel";
 
 export async function getall() {
   return getProfilPictures();
-}
-
-export async function getOneById(id: number): Promise<ProfilPictureModel> {
-  return getOne(id);
-}
-
-export async function deleteOne(id: number) {
-  return deleteProfilPicture(id);
-}
-
-export async function addOne(profilPicture: Omit<ProfilPictureModel, "id">) {
-  return addProfilPicture(profilPicture);
-}
-
-export async function editOne(
-  profilPicture: ProfilPictureModel
-): Promise<{ message: string }> {
-  return patchProfilPicture(profilPicture);
 }
