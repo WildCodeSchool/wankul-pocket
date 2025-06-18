c6009fa0d60b0530a8ece67da95a4eaae5a20acf
Jordan-182 <jordan.p62@outlook.fr>
2025-06-18 20:37:06 +0200

    in the collection page, user can click on a card to open a modal displaying the card in a bigger size and some infos about the card. User can move the card with his mouse and a special effect appears on holo cards

---
+++ src/ui/CardModal.tsx
@@ -0,0 +1,130 @@
+import { CardsModel } from "@/model/CardsModel";
+import Image from "next/image";
+import styles from "./CardModal.module.css";
+
+type CardModalProps = {
+  card: CardsModel;
+  onClose: () => void;
+};
+
+export default function CardModal({ card, onClose }: CardModalProps) {
+  const handleMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const el = event.currentTarget;
+    const elRect = el.getBoundingClientRect();
+
+    const x = event.clientX - elRect.x;
+    const y = event.clientY - elRect.y;
+
+    const midCardWidth = elRect.width / 2;
+    const midCardHeight = elRect.height / 2;
+
+    const angleY = (x - midCardWidth) / 8;
+    const angleX = -(y - midCardHeight) / 8;
+
+    const shadowX = (x - midCardWidth) / 5;
+    const shadowY = (y - midCardHeight) / 5;
+
+    el.children[0].setAttribute(
+      "style",
+      `transform: rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.05); transition: transform 0.1s ease; box-shadow: ${-shadowX}px ${-shadowY}px 30px rgba(0, 0, 0, 0.25)`
+    );
+
+    const holo = el.querySelector(`.${styles.holoLayer}`) as HTMLElement;
+    if (holo) {
+      const posX = (x / elRect.width) * 100;
+      const posY = (y / elRect.height) * 100;
+      holo.style.backgroundPosition = `${posX}% ${posY}%`;
+    }
+  };
+
+  const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    const touch = event.touches[0];
+    const el = event.currentTarget;
+    const elRect = el.getBoundingClientRect();
+
+    const x = touch.clientX - elRect.left;
+    const y = touch.clientY - elRect.top;
+
+    const midX = elRect.width / 2;
+    const midY = elRect.height / 2;
+
+    const angleY = (x - midX) / 8;
+    const angleX = -(y - midY) / 8;
+
+    const shadowX = (x - midX) / 5;
+    const shadowY = (y - midY) / 5;
+
+    el.children[0].setAttribute(
+      "style",
+      `transform: rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.05); transition: transform 0.1s ease; box-shadow: ${-shadowX}px ${-shadowY}px 30px rgba(0, 0, 0, 0.25)`
+    );
+
+    const holo = el.querySelector(`.${styles.holoLayer}`) as HTMLElement;
+    if (holo) {
+      const posX = (x / elRect.width) * 100;
+      const posY = (y / elRect.height) * 100;
+      holo.style.backgroundPosition = `${posX}% ${posY}%`;
+    }
+  };
+
+  const resetMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const el = event.currentTarget;
+    el.children[0].setAttribute(
+      "style",
+      "transform: rotateX(0deg) rotateY(0deg) scale(1); transition: transform 0.3s ease; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1)"
+    );
+    const holo = el.querySelector(`.${styles.holoLayer}`) as HTMLElement;
+    if (holo) {
+      holo.style.backgroundPosition = `50% 50%`;
+    }
+  };
+
+  const handleTouchEnd = (event: React.TouchEvent<HTMLDivElement>) => {
+    const el = event.currentTarget;
+    el.children[0].setAttribute(
+      "style",
+      "transform: rotateX(0deg) rotateY(0deg) scale(1); transition: transform 0.3s ease; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1)"
+    );
+
+    const holo = el.querySelector(`.${styles.holoLayer}`) as HTMLElement;
+    if (holo) {
+      holo.style.backgroundPosition = `50% 50%`;
+    }
+  };
+  return (
+    <section className={styles.modal}>
+      <button onClick={onClose} className={styles.closeBtn}>
+        ✖
+      </button>
+      <div
+        className={styles.card}
+        onMouseMove={handleMove}
+        onMouseLeave={resetMove}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div className={styles.contentCard}>
+          <Image
+            src={card.image_path}
+            alt={card.name}
+            width={300}
+            height={419}
+          />
+          <div className={card.is_holo ? styles.holoLayer : styles.none}></div>
+          <div className={card.is_holo ? styles.illusion : styles.none}></div>
+        </div>
+      </div>
+      <div className={styles.cardInfo}>
+        <h2>{card.name}</h2>
+        <p className={styles.quote}>{card.quote}</p>
+        <p>Rareté : {card.rarity}</p>
+        <p>Clan : {card.clan}</p>
+        {card.quantity === 1 ? (
+          <p>Possédée en {card.quantity} exemplaire</p>
+        ) : (
+          <p>Possédée en {card.quantity} exemplaires</p>
+        )}
+      </div>
+    </section>
+  );
+}
--- src/ui/CollectionContainer.tsx
+++ src/ui/CollectionContainer.tsx
@@ -3,13 +3,19 @@
 import { useCollectionContext } from "@/context/CollectionContext";
 import { CardsModel } from "@/model/CardsModel";
 import Image from "next/image";
+import { useState } from "react";
+import CardModal from "./CardModal";
 import styles from "./CollectionContainer.module.css";
 
 export default function CollectionContainer() {
   const { collection } = useCollectionContext();
+  const [selectedCard, setSelectedCard] = useState<CardsModel | null>(null);
 
   return (
     <section>
+      {selectedCard && (
+        <CardModal card={selectedCard} onClose={() => setSelectedCard(null)} />
+      )}
       <h2 className={styles.season}>Saison 1 : Origins</h2>
       <p className={styles.counter}>
         Cartes obtenues : {collection?.length} / 180
@@ -18,7 +24,13 @@ export default function CollectionContainer() {
         {collection
           ?.filter((card) => card.season === 1)
           .map((card: CardsModel) => (
-            <li key={card.id} className={styles.cardItem}>
+            <li
+              key={card.id}
+              className={styles.cardItem}
+              onClick={() => {
+                setSelectedCard(card);
+              }}
+            >
               <Image
                 src={card.image_path}
                 alt={card.name}
