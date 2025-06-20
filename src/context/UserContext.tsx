d8149f7b8e1b96514c04aead5733381dee7643da
Jordan-182 <jordan.p62@outlook.fr>
2025-06-20 12:24:44 +0200

    refactored user context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -7,14 +7,12 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 type UserContextType = {
   user: UserModel | null;
-  loading: boolean;
-  refreshUser: () => Promise<void>;
+  setUser: React.Dispatch<React.SetStateAction<UserModel | null>>;
 };
 
 const UserContext = createContext<UserContextType>({
   user: null,
-  loading: true,
-  refreshUser: async () => {},
+  setUser: () => {},
 });
 
 export const useUserContext = () => useContext(UserContext);
@@ -22,33 +20,27 @@ export const useUserContext = () => useContext(UserContext);
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
   const [user, setUser] = useState<UserModel | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  const fetchUser = async () => {
-    if (!session?.user?.email) return;
-
-    setLoading(true);
-    try {
-      const data: UserModel = await getOne(session?.user?.email);
-      setUser(data);
-    } catch (err) {
-      console.error("Erreur fetch user context :", err);
-      setUser(null);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   useEffect(() => {
+    const fetchUser = async () => {
+      if (!session?.user?.email) return;
+
+      try {
+        const data = await getOne(session.user.email);
+        setUser(data);
+      } catch (err) {
+        console.error("Erreur fetch user context :", err);
+        setUser(null);
+      }
+    };
+
     if (status === "authenticated") {
       fetchUser();
-    } else if (status !== "loading") {
-      setLoading(false);
     }
   }, [session, status]);
 
   return (
-    <UserContext.Provider value={{ user, loading, refreshUser: fetchUser }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
