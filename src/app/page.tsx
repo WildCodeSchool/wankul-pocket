2e16dbac2e88f55c608ba420fe3eac4b66157865
Salvak613 <jules.clauwaert@gmail.com>
2025-06-26 16:44:09 +0200

    added ouvrir booster components on homepage, deleted the homepage page, it is now on '/'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Homepage</h1>;
+import { HomepageBoosters } from "@/ui/HomepageBoosters";
+import { getall } from "@/service/BoosterService";
+import { BoosterModel } from "@/model/BoosterModel";
+
+export default async function Homepage() {
+  const boosters: BoosterModel[] = await getall();
+  console.log("Boosters fetched:", boosters);
+
+  return (
+    <div>
+      <h1>homepage</h1>
+      <HomepageBoosters boosters={boosters} />
+    </div>
+  );
 }
