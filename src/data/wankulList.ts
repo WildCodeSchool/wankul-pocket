db776b1daec3f58a3a4615023f3264882fdaf35a
Jules Clauwaert <jules.clauwaert@gmail.com>
2025-06-30 17:14:59 +0200

    display random wankuls on any page in desktop, and one wankul in landing page in mobile version

---
+++ src/data/wankulList.ts
@@ -0,0 +1,21 @@
+const Laink = [
+  "/Laink/Laink1.png",
+  "/Laink/Laink2.png",
+  "/Laink/Laink3.png",
+  "/Laink/Laink4.png",
+  "/Laink/Laink5.png",
+];
+
+const Terracid = [
+  "/Terracid/Terracid1.png",
+  "/Terracid/Terracid2.png",
+  "/Terracid/Terracid3.png",
+  "/Terracid/Terracid4.png",
+  "/Terracid/Terracid5.png",
+  "/Terracid/Terracid6.png",
+];
+
+export const Wankul = {
+  Laink: Laink,
+  Terracid: Terracid,
+};
