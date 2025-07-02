668b0812c292b4a7c5910993466ec947fed70d6f
Jordan-182 <jordan.p62@outlook.fr>
2025-07-02 17:23:22 +0200

    still css to do

--- src/ui/HomepageTrade.tsx
+++ src/ui/HomepageTrade.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { TradeModel } from "@/model/TradeModel";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./HomepageTrade.module.css";
 
@@ -9,22 +10,52 @@ interface Props {
 }
 
 export default function HomepageTrade({ trade }: Props) {
-  if (trade === null) {
+  console.log(trade);
+  if (trade === null || undefined) {
     return (
       <Link href={`/echange`} className={styles.container}>
         <div className={styles.trade}>
           <h2 className={styles.title}>Echanges</h2>
+          <Image
+            src={"./notification.png"}
+            alt="Echange"
+            width={50}
+            height={50}
+          />
+        </div>
+      </Link>
+    );
+  } else {
+    return (
+      <Link href={`/echange`} className={styles.container}>
+        <div className={styles.trade}>
+          <h2 className={styles.title}>Echanges</h2>
+          <Image
+            src="/notification.png"
+            alt="Notification d'échange"
+            height={20}
+            width={20}
+            className={styles.notification}
+          />
+          <div className="cardsContainer">
+            <Image
+              src={trade?.requested_card_img}
+              alt="Carte proposée"
+              height={70}
+              width={50}
+              className={styles.card}
+            />
+            <Image src="/tradeIcon.png" alt="Echange" width={24} height={24} />
+            <Image
+              src={trade?.offered_card_img}
+              alt="Carte proposée"
+              height={70}
+              width={50}
+              className={styles.card}
+            />
+          </div>
         </div>
       </Link>
     );
   }
-
-  return (
-    <Link href={`/echange`} className={styles.container}>
-      <div className={styles.trade}>
-        <h2 className={styles.title}>Echanges</h2>
-        <p>{trade?.from_username}</p>
-      </div>
-    </Link>
-  );
 }
