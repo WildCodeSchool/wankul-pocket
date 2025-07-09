c0484692e79d859868fa177f5a4027efcf3412c2
Salahedine Imoussaine <salah.imoussaine@gmail.com>
2025-07-09 15:42:00 +0200

    fix: correct SQL syntax for inserting profil_picture entries

--- script/seed.ts
+++ script/seed.ts
@@ -81,14 +81,14 @@ const seed = async () => {
 
     await db.query(`
   INSERT INTO profil_picture (id, image_path) VALUES
-  "perso1.png",
-  "perso2.png",
-  "perso3.png",
-  "perso4.png",   
-  "perso5.png",
-  "perso6.png",
-  "perso7.png",
-  "perso8.png"
+  ("/perso1.png)",
+  ("/perso2.png)",
+  ("/perso3.png)",
+  ("/perso4.png)",   
+  ("/perso5.png)",
+  ("/perso6.png)",
+  ("/perso7.png)",
+  ("/perso8.png)"
 `);
 
     await db.end();
