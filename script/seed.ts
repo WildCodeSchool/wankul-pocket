5d60cb9a9badcefd70739bfcb0420cf26c8a3fc9
Jordan-182 <jordan.p62@outlook.fr>
2025-07-10 10:10:48 +0200

    fixed all errors that blocked the build, now the build is working on local and everything is up to date on this branch

--- script/seed.ts
+++ script/seed.ts
@@ -96,16 +96,7 @@ const seed = async () => {
     await db.query("ALTER TABLE profil_picture AUTO_INCREMENT = 1");
 
     await db.query(`
-  INSERT INTO profil_picture (image_path) VALUES
-  ("/perso1.png)",
-  ("/perso2.png)",
-  ("/perso3.png)",
-  ("/perso4.png)",   
-  ("/perso5.png)",
-  ("/perso6.png)",
-  ("/perso7.png)",
-  ("/perso8.png)"
-`);
+      INSERT INTO profil_picture (image_path) VALUES ("/profilpic/perso1.png"), ("/profilpic/perso2.png"), ("/profilpic/perso3.png"), ("/profilpic/perso4.png"), ("/profilpic/perso5.png"), ("/profilpic/perso6.png"), ("/profilpic/perso7.png"), ("/profilpic/perso8.png")`);
 
     await db.end();
     console.log("🌱 Database seeded successfully");
--- src/app/booster/page.tsx
+++ src/app/booster/page.tsx
@@ -1,11 +1,20 @@
-import BoosterSelection from "@/ui/BoosterSelection";
-import { getBoosters } from "@/lib/getBoosters";
+export const dynamic = "force-dynamic";
 
+import { getBoosters } from "@/lib/getBoosters";
+import BoosterSelection from "@/ui/BoosterSelection";
+
+import { boostersMessages } from "@/data/responseMessages";
+import { notFound } from "next/navigation";
 import styles from "./boosterSelection.module.css";
 
-const boosters = await getBoosters();
-
 export default async function BoosterPage() {
+  let boosters;
+  try {
+    boosters = await getBoosters();
+  } catch (err) {
+    console.error(boostersMessages.error, err);
+    notFound();
+  }
   return (
     <div className={styles.container}>
       <BoosterSelection boosters={boosters} />
